53af821a85b93504225109c3be29b2fc000a751b
jordan <jordan.anderson@kellpro.com>
2018-06-11 16:26:10 -0500

    combine email and username query

--- controllers/sign-up.php
+++ controllers/sign-up.php
@@ -23,33 +23,20 @@ if (!empty($_POST['email'])) {
     }
   }
 
-  //check if email already exists
-  $query = $database->prepare("SELECT * FROM users WHERE lower(email) = :email");
-  $query->bindValue(':email', $postValues->email);
-  $query->execute();
+  $checkForUser = $database->prepare("SELECT * FROM users WHERE lower(email) = :email OR lower(username) = :username");
+  $checkForUser->bindValue(':email', $postValues->email);
+  $checkForUser->bindValue(':username', $postValues->username);
+  $checkForUser->execute();
+  $user = $checkForUser->fetch();
 
-  $results = $query->fetch();
-
-  if (!empty($results)) {
+  if (!empty($user->email) && $user->email === $postValues->email) {
     $emailErrorMessage = 'That email is in use. You may already have an account.';
     return;
   }
 
-
-  if (!empty($postValues->username)) {
-    //check if username already exists
-    $query = $database->prepare("SELECT * FROM users WHERE lower(username) = :username");
-    $query->bindValue(':username', $postValues->username);
-    $query->execute();
-
-    $results = $query->fetch();
-
-    if (!empty($results)) {
-      $usernameErrorMessage = 'Sorry! Looks like that username is taken.';
-      return;
-    }
-  } else {
-    $postValues->username = null;
+  if (!empty($user->username) && $user->username === $postValues->username) {
+    $usernameErrorMessage = 'Sorry! Looks like that username is taken.';
+    return;
   }
 
   if (empty($postValues->first_name)) {
@@ -69,15 +56,15 @@ if (!empty($_POST['email'])) {
     return;
   }
 
-  $insertStatement = $database->prepare("INSERT INTO users (email, first_name, last_name, username, password, signed_up_date) VALUES (:email, :first_name, :last_name, :username, :password, now()) RETURNING id");
-  $insertStatement->bindValue(':email', $postValues->email);
-  $insertStatement->bindValue(':first_name', $postValues->first_name);
-  $insertStatement->bindValue(':last_name', $postValues->last_name);
-  $insertStatement->bindValue(':username', $postValues->username);
-  $insertStatement->bindValue(':password', $postValues->password);
-  $insertStatement->execute();
+  $insertUser = $database->prepare("INSERT INTO users (email, first_name, last_name, username, password, signed_up_date) VALUES (:email, :first_name, :last_name, :username, :password, now()) RETURNING id");
+  $insertUser->bindValue(':email', $postValues->email);
+  $insertUser->bindValue(':first_name', $postValues->first_name);
+  $insertUser->bindValue(':last_name', $postValues->last_name);
+  $insertUser->bindValue(':username', $postValues->username);
+  $insertUser->bindValue(':password', $postValues->password);
+  $insertUser->execute();
 
-  $userId = $insertStatement->fetchColumn();
+  $userId = $insertUser->fetchColumn();
   $_SESSION['user_id'] = $userId;
 
   header('Location: /user/dashboard');
