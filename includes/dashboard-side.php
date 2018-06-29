07963be52f43595fd965f90d0a99a82f84650026
jordan <jordan.anderson@kellpro.com>
2018-06-29 16:08:17 -0500

    add nav tag to dashboard

--- includes/dashboard-side.php
+++ includes/dashboard-side.php
@@ -1,16 +1,18 @@
-<div id="dashboard-tabs">
-  <ul>
-    <li>
-      <a href="/user/dashboard" id="<?= ($resquestedPage === "user/dashboard" ? "current-link" : " ") ?>">dashboard</a>
-    </li>
-    <li>
-      <a href="/user/photos" id="<?= ($resquestedPage === "user/photos" ? "current-link" : " ") ?>">your photos</a>
-    </li>
-    <li>
-      <a href="/user/upload-photo" id="<?= ($resquestedPage === "user/upload-photo" ? "current-link" : " ") ?>">upload a photo</a>
-    </li>
-    <li>
-      <a href="">account settings</a>
-    </li>
-  </ul>
+<div id="dashboard-navbar">
+  <nav>
+    <ul>
+      <li>
+        <a href="/user/dashboard" id="<?= ($resquestedPage === "user/dashboard" ? "current-link" : " ") ?>">dashboard</a>
+      </li>
+      <li>
+        <a href="/user/photos" id="<?= ($resquestedPage === "user/photos" ? "current-link" : " ") ?>">your photos</a>
+      </li>
+      <li>
+        <a href="/user/upload-photo" id="<?= ($resquestedPage === "user/upload-photo" ? "current-link" : " ") ?>">upload a photo</a>
+      </li>
+      <li>
+        <a href="">account settings</a>
+      </li>
+    </ul>
+  </nav>
 </div>
