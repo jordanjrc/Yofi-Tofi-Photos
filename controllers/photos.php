0930d537d4a377faef76a848adcdd19fb006a246
jordan <jordan.anderson@kellpro.com>
2018-07-17 13:42:18 -0500

    change gallery layout

--- controllers/photos.php
+++ controllers/photos.php
@@ -1,13 +1,27 @@
 <?php
 $getPhotos = $database->prepare("SELECT photos.*, users.first_name, users.last_name, users.username FROM photos JOIN users on photos.user_id = users.id ORDER BY upload_date DESC");
 $getPhotos->execute();
-
 $photos = $getPhotos->fetchAll();
 
-$getUsers = $database->prepare("SELECT distinct on (users.id) users.id, users.first_name, users.last_name, users.username, photos.filename, photos.title FROM users
-left JOIN photos on photos.user_id = users.id
-ORDER BY users.id DESC, upload_date DESC
-");
+$getUsers = $database->prepare(
+  "SELECT distinct on (users.id) users.id, users.first_name, users.last_name, users.username, photos.filename, photos.title
+  FROM users
+  LEFT JOIN photos on photos.user_id = users.id
+  ORDER BY users.id DESC, upload_date DESC"
+);
 $getUsers->execute();
-
 $users = $getUsers->fetchAll();
+
+if (isset($_GET['user_id'])) {
+  $userId = $_GET['user_id'];
+
+  $getUserInfo = $database->prepare("SELECT username, first_name, last_name FROM users WHERE id = :user_id");
+  $getUserInfo->bindValue(':user_id', $userId);
+  $getUserInfo->execute();
+  $userInfo = $getUserInfo->fetch();
+
+  $getPhotos = $database->prepare("SELECT photos.*, users.first_name, users.last_name, users.username FROM photos JOIN users on photos.user_id = users.id WHERE photos.user_id = :user_id ORDER BY upload_date DESC");
+  $getPhotos->bindValue(':user_id', $userId);
+  $getPhotos->execute();
+  $photos = $getPhotos->fetchAll();
+}
--- views/photos.php
+++ views/photos.php
@@ -1,19 +1,22 @@
 <section class="color-block-green"></section>
 
-<div id="photos-header">
-  <nav>
-    <h2>The Photo Gallery</h2>
-    <ul>
-      <li><a href="/photos" id="<?= ($_SERVER['REQUEST_URI'] === "/photos" ? "current-link" : " ") ?>">all photos</a></li>
-      <li><a href="/photos?users" id="<?= ($_SERVER['REQUEST_URI'] === "/photos?users" ? "current-link" : " ") ?>">all users</a></li>
-    </ul>
-  </nav>
-</div>
+<? if (!isset($_GET['user_id'])) { ?>
+  <section id="photos-header-photo"></section>
+  <div id="photos-header">
+    <nav>
+      <h2>The Photo Gallery</h2>
+      <ul>
+        <li><a href="/photos" id="<?= ($_SERVER['REQUEST_URI'] === "/photos" ? "current-link" : " ") ?>">all photos</a></li>
+        <li><a href="/photos?users" id="<?= ($_SERVER['REQUEST_URI'] === "/photos?users" ? "current-link" : " ") ?>">all users</a></li>
+      </ul>
+    </nav>
+  </div>
+<? } ?>
 
 <? if (isset($_GET['users'])) { ?>
     <div id="photo-gallery-users">
       <? foreach ($users as $user) { ?>
-      <a href="profile?user_id=<?= $user->id ?>">
+      <a href="photos?user_id=<?= $user->id ?>">
         <div>
           <img src="/images/<?= (!empty($user->filename) ? 'original/' . $user->filename : 'happy-coffee.jpg' ) ?>" alt="<?= $user->title ?>">
           <p><?= $user->first_name . ' ' . $user->last_name ?></p>
@@ -23,47 +26,58 @@
     <? } ?>
     </div>
 <? } else { ?>
-    <div id="photo-gallery">
-    <? foreach ($photos as $photo) { ?>
-      <div id="image-card">
-        <img id="photo-gallery-image<?= $photo->id ?>" src="/images/original/<?= $photo->filename ?>" alt="<?= $photo->title ?>">
-        <p><?= $photo->title ?></p>
-        <p><a href="profile?user_id=<?= $photo->user_id?>">@<?= $photo->username ?></a></p>
-      </div>
-
-      <div id="imageModal" class="modal">
-        <!-- The Close Button -->
-        <span id="close">&times;</span>
-        <!-- Modal Content (The Image) -->
-        <img id="modal-image">
-        <!-- Modal Caption (Image Text) -->
-        <div id="caption"></div>
-      </div>
-
-      <script>
-      var modal = document.getElementById('imageModal');
-      var image = document.getElementById('photo-gallery-image<?= $photo->id ?>');
-      var modalImage = document.getElementById('modal-image');
-      var captionText = document.getElementById('caption');
-
-      image.onclick = function() {
-        modal.style.display = "block";
-        modalImage.src = this.src;
-        captionText.innerHTML = this.alt;
-      }
-
-      var closeSpan = document.getElementById('close');
-
-      closeSpan.onclick = function() {
-        modal.style.display = "none";
-      }
-      </script>
-
+<? if (isset($_GET['user_id'])) { ?>
+  <div id="profile-header">
+    <h2><?= $userInfo->first_name . ' ' . $userInfo->last_name ?></h2>
+    <p id="user-handle"><?= (!empty($userInfo->username) ? '@' . $userInfo->username : '') ?></p>
+    <? if (empty($photos)) { ?>
+      <p>No photos upload yet!</p>
     <? } ?>
-    <div id="image-card" class="hidden"></div>
-    <div id="image-card" class="hidden"></div>
-    <div id="image-card" class="hidden"></div>
-    <div id="image-card" class="hidden"></div>
+    <p><a href="photos">back to gallery</a></p>
+  </div>
+<? } ?>
+
+  <div id="photo-gallery">
+  <? foreach ($photos as $photo) { ?>
+    <div id="image-card">
+      <img id="photo-gallery-image<?= $photo->id ?>" src="/images/original/<?= $photo->filename ?>" alt="<?= $photo->title ?>">
+      <p><?= $photo->title ?></p>
+      <p><a href="photos?user_id=<?= $photo->user_id?>">@<?= $photo->username ?></a></p>
     </div>
+
+    <div id="imageModal" class="modal">
+      <!-- The Close Button -->
+      <span id="close">&times;</span>
+      <!-- Modal Content (The Image) -->
+      <img id="modal-image">
+      <!-- Modal Caption (Image Text) -->
+      <div id="caption"></div>
+    </div>
+
+    <script>
+    var modal = document.getElementById('imageModal');
+    var image = document.getElementById('photo-gallery-image<?= $photo->id ?>');
+    var modalImage = document.getElementById('modal-image');
+    var captionText = document.getElementById('caption');
+
+    image.onclick = function() {
+      modal.style.display = "block";
+      modalImage.src = this.src;
+      captionText.innerHTML = this.alt;
+    }
+
+    var closeSpan = document.getElementById('close');
+
+    closeSpan.onclick = function() {
+      modal.style.display = "none";
+    }
+    </script>
+
+  <? } ?>
+  <div id="image-card" class="hidden"></div>
+  <div id="image-card" class="hidden"></div>
+  <div id="image-card" class="hidden"></div>
+  <div id="image-card" class="hidden"></div>
+  </div>
 <? } ?>
 
